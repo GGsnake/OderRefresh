69d7ed9a687e40f940212d50422f35da714632bc
GGsnake <ljp462693266@outlook.com>
2018-11-17 15:44:30 +0800

    新增扫描订单到数据库

--- spring-boot-scheduler/src/main/java/com/neo/dao/PddOderDao.java
+++ spring-boot-scheduler/src/main/java/com/neo/dao/PddOderDao.java
@@ -1,8 +1,13 @@
 package com.neo.dao;
 
+import com.neo.jsonbean.PddOderBean;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * Created by liujupeng on 2018/11/16.
  */
+@Mapper
 public interface PddOderDao {
-
+   void addOder(@Param("oder") PddOderBean pddOderBean);
 }
--- spring-boot-scheduler/src/main/java/com/neo/task/SchedulerTask.java
+++ spring-boot-scheduler/src/main/java/com/neo/task/SchedulerTask.java
@@ -1,15 +1,24 @@
 package com.neo.task;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.neo.Utils.EveryUtils;
 import com.neo.Utils.HttpRequest;
+import com.neo.dao.PddOderDao;
+import com.neo.jsonbean.PddOderBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -31,52 +40,27 @@ public class SchedulerTask {
     private Integer RANGE;
     private int count = 0;
     private final static Logger logger = LoggerFactory.getLogger(SchedulerTask.class);
-    static  Long sy=1542360350l;
+    static Long sy = 1542297601l;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-    //订单通知
-    @Scheduled(fixedDelay = 10000)
-    private void process() {
-        Long sum=sy+10000;
-        String res = null;
-        String type = "pdd.ddk.order.list.increment.get";
-        Long time =System.currentTimeMillis() / 1000;
-        String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
-
-        SortedMap<String, String> urlSign = new TreeMap<>();
-        urlSign.put("client_id", KEY);
-        urlSign.put("type", type);
-        urlSign.put("timestamp", timestamp);
-        urlSign.put("end_update_time", timestamp);
-        urlSign.put("start_update_time", sy.toString());
-        urlSign.put("data_type", "JSON");
-        urlSign.put("sign", EveryUtils.pddSign(urlSign, SECRET));
-        //        urlSign.put("access_token", ACCESS_TOKEN);
-        try {
-            res = HttpRequest.sendPost("https://gw-api.pinduoduo.com/api/router", urlSign);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        logger.warn(res);
-        logger.warn(String.valueOf(sy));
-        logger.warn(String.valueOf(sum));
-
-        sy=sy+10000;
-    }
-
-//    //订单落库
-//    @Scheduled(fixedRate = 1800000)
-//    public void reportCurrentTime() {
+    @Autowired
+    private PddOderDao pddOderDao;
+//    //订单通知
+//    @Scheduled(fixedDelay = 2000)
+//    private void process() {
+//        Long sum = sy + 86400;
 //        String res = null;
-//        String stattimestamp = "1542335895";
 //        String type = "pdd.ddk.order.list.increment.get";
+//        Long time = System.currentTimeMillis() / 1000;
 //        String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
+//
 //        SortedMap<String, String> urlSign = new TreeMap<>();
 //        urlSign.put("client_id", KEY);
 //        urlSign.put("type", type);
 //        urlSign.put("timestamp", timestamp);
-//        urlSign.put("end_update_time", timestamp);
-//        urlSign.put("start_update_time", stattimestamp);
+//        urlSign.put("end_update_time", timestamp.toString());
+//        urlSign.put("start_update_time","0");
 //        urlSign.put("data_type", "JSON");
 //        urlSign.put("sign", EveryUtils.pddSign(urlSign, SECRET));
 //        //        urlSign.put("access_token", ACCESS_TOKEN);
@@ -85,7 +69,53 @@ public class SchedulerTask {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+//
 //        logger.warn(res);
-//    }
+//        logger.warn(String.valueOf(sy));
+//        logger.warn(String.valueOf(sum));
+//        JSONArray jsonObject = JSONObject.parseObject(res).getJSONObject("order_list_get_response").getJSONArray("order_list");
+//        HashOperations hashOperations = redisTemplate.opsForHash();
+//        for (int i = 0; i < jsonObject.size(); i++) {
+//            JSONObject o = (JSONObject) jsonObject.get(i);
+//            String p_id = o.getString("p_id");
+//            String order_sn = o.getString("order_sn");
+//            String redis_key = "pdd_pid:" + p_id;
+//            hashOperations.put(redis_key, p_id + ":" + order_sn, o.toJSONString());
+//        }
+////        List values = redisTemplate.opsForHash().size();
+////        logger.warn(values.toString());
+//
+//        sy = sy + 10000;
+//        }
 
-}
+    //订单落库
+    @Scheduled(fixedRate = 19000)
+    public void reportCurrentTime() {
+        String res = null;
+        String stattimestamp = "1542335895";
+        String type = "pdd.ddk.order.list.increment.get";
+        String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
+        SortedMap<String, String> urlSign = new TreeMap<>();
+        urlSign.put("client_id", KEY);
+        urlSign.put("type", type);
+        urlSign.put("timestamp", timestamp);
+        urlSign.put("end_update_time", timestamp);
+        urlSign.put("start_update_time", stattimestamp);
+        urlSign.put("data_type", "JSON");
+        urlSign.put("sign", EveryUtils.pddSign(urlSign, SECRET));
+        //        urlSign.put("access_token", ACCESS_TOKEN);
+        try {
+            res = HttpRequest.sendPost("https://gw-api.pinduoduo.com/api/router", urlSign);
+            JSONArray jsonObject = JSONObject.parseObject(res).getJSONObject("order_list_get_response").getJSONArray("order_list");
+            HashOperations hashOperations = redisTemplate.opsForHash();
+            for (int i = 0; i < jsonObject.size(); i++) {
+                JSONObject o = (JSONObject) jsonObject.get(i);
+                pddOderDao.addOder(o.toJavaObject(PddOderBean.class));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.warn(res);
+    }
+
+    }
